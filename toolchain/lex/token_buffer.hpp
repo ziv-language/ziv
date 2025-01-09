bb9a48f6ba0eab9ae1c8eeb3b0b45524e0a4bd23
Shobhit <shobhit32t@gmail.com>
2025-01-10 02:32:57 +0530

    run clang-format to update style

--- toolchain/lex/token_buffer.hpp
+++ toolchain/lex/token_buffer.hpp
@@ -37,7 +37,7 @@ public:
               spelling(spelling_value),
               line(line),
               column(column),
-              filename(filename){};
+              filename(filename) {};
 
         Token(const Token& token)
             : kind(token.kind),
@@ -45,7 +45,7 @@ public:
               spelling(spelling_value),
               line(token.line),
               column(token.column),
-              filename(token.filename){}
+              filename(token.filename) {}
 
         static Token create_empty(TokenKind kind = TokenKind::Sof()) {
             return Token(kind, "", "", 0, 0);
